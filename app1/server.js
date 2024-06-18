ac55da2b56f46d1d4e50a3bc80697923a7c0f3ee
abhijeetdrapcode <abhijeet@drapcode.com>
2024-06-18 13:03:26 +0530

    Removed Socket io code that saved user data

--- app1/server.js
+++ app1/server.js
@@ -7,18 +7,8 @@ const path = require('path');
 const cors = require('cors');
 const token = require('./middleware/token');
 
-//Disable the three line below to remove the user click data functionality
-const socketIO = require('socket.io');
-const mongoose = require('./db/mongodb');
-const routes = require('./routes/clickDataRoute');
-//Two more lines need to be commented below
-
 const app = express();
 const server = http.createServer(app);
-
-//This line also need to be disabled
-const io = socketIO(server);
-//There is one more line of code that need to be removed 
 
 const PORT = process.env.PORT;
 
@@ -49,9 +39,6 @@ app.get('/', (req, res) => {
   res.send("Hello World, This is for testing the server!");
 });
 
-//This line of code also need to be removed
-app.use('/api', routes(io));
-//Commenting ends here
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
